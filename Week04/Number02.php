58da8d6e6d0a203fb3583d269e8a04442c8dc711
1515148 <s.jury@rgu.ac.uk>
2016-02-24 15:50:46 +0000

    n

---
+++ Week04/Number02.php
@@ -0,0 +1,15 @@
+<?php
+$myage= OLDGIT
+
+if($myage >=21) {
+    echo "You can buy specs, mugs, and sausage rolls";
+}
+if($myage >=18) {
+    echo "You can buy specs and mugs";
+}
+if($myage >=16) {
+    echo "You can buy specs";
+}
+elseif
+    echo "You can buy anything you like.....";
+}
