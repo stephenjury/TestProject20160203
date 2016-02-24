31f1f9208d529b222f9b298da5272160bcd0bb63
1515148 <s.jury@rgu.ac.uk>
2016-02-24 16:37:54 +0000

    n

--- Week04/Number04.php
+++ Week04/Number04.php
@@ -1,11 +1,11 @@
 <?php
 $myArray = array("do", "re", "mi"); //Declares the array
 
-echo $myArray[0] //outputs "do"
+echo $myArray[0]; //outputs "do"
 
 $myArray[1] = "la"; //modifies position 1 (re)
 
-echo $myArray[1] //outputs "la"
+echo $myArray[1]; //outputs "la"
 
 unset($myArray[2]); // removes the array in position 2
 
