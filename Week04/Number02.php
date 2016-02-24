7fbd2d11e44e53bdc8303782b56e872cec11d1e3
1515148 <s.jury@rgu.ac.uk>
2016-02-24 16:20:45 +0000

    n

--- Week04/Number02.php
+++ Week04/Number02.php
@@ -7,11 +7,11 @@ if($myage >=21) {
     echo "You can buy specs, mugs, and sausage rolls";
 }
 
-if($myage >=18) {
+elseif($myage >=18) {
     echo "You can buy specs and mugs";
 }
 
-if($myage >=16) {
+elseif($myage >=16) {
     echo "You can buy specs";
 }
 else {
