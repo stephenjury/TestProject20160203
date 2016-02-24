9a4413fef0665f23e6acbf14ed1eace2530ba254
1515148 <s.jury@rgu.ac.uk>
2016-02-24 16:18:19 +0000

    n

--- Week04/Number02.php
+++ Week04/Number02.php
@@ -1,20 +1,20 @@
 <?php
-/*
+
 $oldgit = 65;
-$myage= $oldgit;
+$myage = $oldgit;
 
 if($myage >=21) {
     echo "You can buy specs, mugs, and sausage rolls";
 }
+
 if($myage >=18) {
     echo "You can buy specs and mugs";
 }
+
 if($myage >=16) {
     echo "You can buy specs";
 }
 else {
     echo "You can buy anything you like.....";
-} */
-
-echo "blarg";
+}
 ?>
