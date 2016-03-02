a813df62ab73d1b3d75e3e42cec8a8c6470a7624
1515148 <s.jury@rgu.ac.uk>
2016-03-02 15:37:16 +0000

    n

---
+++ Week05/connection.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-west-d.cloudapp.net');
+define('DB_USERNAME', 'bc5a8d4c3bd955');
+define('DB_PASSWORD', '6e11bf33');
+define('DB_DATABASE', 'db1515148');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
