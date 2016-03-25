0a094a0ccb253f334b5e9d39029405d9dcbf97a0
Admin <a@a.com>
2016-03-25 05:33:46 +0000

    1

--- Project-1/login/submit.php
+++ Project-1/login/submit.php
@@ -19,7 +19,7 @@ if(isset($_POST["submit"]))
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
     if(mysqli_num_rows($result) == 1)
     {
-        $email1 = """ . $email . """;
+        $email1 = " . $email . ";
         $msg1 = "Sorry...This email ";
         $msg2 = " already exists...";
         $msg = $msg1 . $email1 . $msg2;
