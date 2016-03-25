abbc0dc140e0c77742e4769b97330a207cecedbc
Admin <a@a.com>
2016-03-25 22:29:33 +0000

    1

--- Project-1/login/reportbug.php
+++ Project-1/login/reportbug.php
@@ -66,7 +66,7 @@
 					</td>
 					<td>
 						<!-- <input type="checkbox" name="fixed" value="1" /> -->
-						<input name="fixed" type="text" class="input" size="5" />
+						<input name="fixed" type="text" class="input" size="25" />
 					</td>
 				</tr>
 				<tr>
