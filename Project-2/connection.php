4b1516d269f5f99674b0a267232876362b84707e
Admin <a@a.com>
2016-04-27 15:22:48 +0100

    1

--- Project-2/connection.php
+++ Project-2/connection.php
@@ -4,5 +4,8 @@ define('DB_USERNAME', 'ba5da06c642e24');
 define('DB_PASSWORD', 'ae90ab25');
 define('DB_DATABASE', 'PhotoComment');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if($db->connect_errno) {
+    echo "Failed to connect to MySQL:(" . $db->connect_error. ")". $db->connect_error;
+}
 
 ?>
