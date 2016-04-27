5aecd005ad8d09d01aa08cbf87d1aae6b3356995
Admin <a@a.com>
2016-04-27 16:32:05 +0100

    1

--- Project-2/addcomment.php
+++ Project-2/addcomment.php
@@ -10,16 +10,22 @@ if(isset($_POST["submit"]))
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
+    //Clean Stuff
+    $clean_desc=mysqli_real_escape_string($db,$desc);
+    $clean_photoID=mysqli_real_escape_string($db,$photID);
+    $clean_name=mysqli_real_escape_string($db,$name);
+
+
+    $sql="SELECT userID FROM users WHERE username='$clean_name'";
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
     if(mysqli_num_rows($result) == 1) {
         //echo $name." ".$email." ".$password;
         $id = $row['userID'];
-        $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
+        $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$clean_desc',now(),'$clean_photoID','$clean_id')";
         $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
         if ($query) {
-            $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
+            $msg = "Thank You! comment added. click <a href='photo.php?id=".$clean_photoID."'>here</a> to go back";
         }
     }
     else{
