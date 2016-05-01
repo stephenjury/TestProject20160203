bffdd7aa8e7723f44fed71b885a80c64a63709b9
Admin <a@a.com>
2016-05-01 17:58:48 +0100

    1

--- Project-2/index.php
+++ Project-2/index.php
@@ -25,7 +25,7 @@
             <label>Username:</label><br>
             <input type="text" name="username" placeholder="username" /><br><br>
             <label>Password:</label><br>
-            <input type="password" name="password" placeholder="password" />  <br><br>
+            <input type="password" name="password" placeholder="password" autocomplete="off"/>  <br><br>
             <input type="submit" name="submit" value="Login" />
         </form>
         <div class="error"><?php echo $error;?></div>
